d52f150e5ab88f36535e6ca076fcd9270fdfa1e3
MagoroGraphics <magoro.graphics@gmail.com>
2022-07-20 10:53:00 +0100

    ProfileItem refactor

--- src/components/ProfileItem.js
+++ src/components/ProfileItem.js
@@ -3,34 +3,34 @@ import styled from 'styled-components';
 
 
 const ListProfileItemStyled = styled.li`
-display:flex;
-flex-direction:row;
-font-weight: bold;
-list-style-type: none;
-font-size: 2vh;
-color: #FFFFFF;
-cursor: pointer;
-font-family: 'Fresh-Steak';
-&:hover{
-    background-color: #FCE74A;
-    color: #FF6666;
-}
-&:focus{
-    color: #FFFFFF ;
-}
-padding: 1.5vh;
-border-radius: 0.5vh;
+  display:flex;
+  flex-direction:row;
+  font-weight: bold;
+  list-style-type: none;
+  font-size: 2vh;
+  color: #FFFFFF;
+  cursor: pointer;
+  font-family: 'Fresh-Steak';
+  &:hover{
+      background-color: #FCE74A;
+      color: #FF6666;
+  }
+  &:focus{
+      color: #FFFFFF ;
+  }
+  padding: 1.5vh;
+  border-radius: 0.5vh;
 `
 
 const ProfileAvatarImageStyled = styled.img`
-max-height:5vh;
-margin:1vh;
+  max-height:5vh;
+  margin:1vh;
 `
 
 const ProfileNameWrapper = styled.div`
-display:flex;
-flex-direction:column;
-justify-content:center;
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
 `
 
 const ProfileItem = ({profile, onProfileClick, setActive, active}) => {
@@ -45,7 +45,13 @@ const ProfileItem = ({profile, onProfileClick, setActive, active}) => {
 
   return (
   <>
-    <ListProfileItemStyled onClick={handleClick} style={profile.id === active ? {backgroundColor: '#FF6666' , color:'white'} : null}> <ProfileAvatarImageStyled src={`http://localhost:8080/${profile.avatar}`}/><ProfileNameWrapper>{profile.name}</ProfileNameWrapper></ListProfileItemStyled>
+    <ListProfileItemStyled 
+      onClick={handleClick} 
+      style={profile.id === active ? {backgroundColor: '#FF6666' , color:'white'} : null}
+    > 
+      <ProfileAvatarImageStyled src={`http://localhost:8080/${profile.avatar}`}/>
+      <ProfileNameWrapper>{profile.name}</ProfileNameWrapper>
+    </ListProfileItemStyled>
   </>
   )
 }
